c36c86b986465e5ac355153662a2b0ccd268b2e1
SamujitDas <samujit.das@gmail.com>
2021-01-10 11:39:17 +0530

    First build

---
+++ build/precache-manifest.fe9f296c7dd2e48193cc9b7491c2ff42.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d6b4662445c535c3c0776fe3777608",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a70242d1a87650d7ae0",
+    "url": "/static/css/main.468e0688.chunk.css"
+  },
+  {
+    "revision": "d62fb0840a8462cdb8d6",
+    "url": "/static/js/2.58e2c180.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.58e2c180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a70242d1a87650d7ae0",
+    "url": "/static/js/main.dbb53550.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "e9aab65437d956213448d56a5a92d157",
+    "url": "/static/media/activate.e9aab654.PNG"
+  },
+  {
+    "revision": "c4deee076f23426c4a180ccb4fa36eb7",
+    "url": "/static/media/core.c4deee07.PNG"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "925ae868dd7519640d44d82055015a6d",
+    "url": "/static/media/main1.925ae868.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  },
+  {
+    "revision": "73c0f363cbf883c804cfd4e6cc3432a4",
+    "url": "/static/media/sam.73c0f363.jpg"
+  }
+]);
