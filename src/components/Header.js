d35514d1fb91a069c132276b5bf31f8b94071757
SamujitDas <samujit.das@gmail.com>
2021-01-10 16:18:50 +0530

    Removed footer from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -65,7 +65,7 @@ const Header = () => {
       
     </Box>
       {/* <Drawer> */}
-        <Footer className = {classes.footer}/>
+        {/* <Footer className = {classes.footer}/> */}
       {/* </Drawer> */}
     </React.Fragment>
   );
